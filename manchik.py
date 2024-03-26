c74b086f4b8f02549bfc0e75d04d3b8cec561cdf
psyberd1001 <108468415+psyberd1001@users.noreply.github.com>
2024-03-26 23:21:35 +0300

    Add files via upload

--- manchik.py
+++ manchik.py
@@ -1,10 +1,9 @@
 import re
+
 list_1 = open('C:/phytonProjects/lesson0/Homework/text3.txt', 'w', encoding='utf-8')
 with open('text.txt', 'r', encoding='utf-8') as f:
-    #print("utf-8")
     text = f.read()
     patn = re.sub(r"[\([{«,.:;—»})\]]","", text)
-    #print(patn)
     list_1.write(patn)
     list_1.close()
     f.close()
@@ -12,22 +11,17 @@ with open('text.txt', 'r', encoding='utf-8') as f:
 def Sortirovka(*args):
 
     list_3 = open('text4.txt', 'w+', encoding='utf-8')
-    f1_text = list_3.read()
-    list_3.close()
+
     with open('text3.txt', 'r+', encoding='utf-8') as list_2:
         f_text = list_2.read()
-        list_2.close()
-        for number in f_text.split():
-            if number in f1_text.split():
-                continue
-            else:
-                f1_text.
-        return f1_text
+        print(type(f_text))
+        f_text_1 = f_text.lower()
+        f_text_2 = f_text_1.split()
+        f_text_3 = list(set(f_text_2))
+        for items in f_text_3:
+            list_3.write('%s\n' % items)
+    list_3.close()
 
 
 
-Sortirovka(patn)
-#print(list_2)
-# print()
-# print()
-# print(Anton_loh(list_2))
+Sortirovka()
